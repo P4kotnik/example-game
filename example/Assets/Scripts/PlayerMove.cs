00c5ae68c89f36eff4e2e858c6cba9d7a5a2338d
P4kotnik <michalkuciak4@gmail.com>
2021-12-01 04:07:10 +0100

    basic move

---
+++ example/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    [Header("Atributs")]
+    public float speed = 10f;
+    public float gravity = -9.81f;
+
+    [Header("Jump, Gravity")]
+    public Transform groundCheck;
+    public bool isGround;
+    public float groundDistance = 0.4f;
+    public float maxJumpHeight = 2f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (isGround && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(maxJumpHeight * -2 * gravity);
+        }
+    }
+}
